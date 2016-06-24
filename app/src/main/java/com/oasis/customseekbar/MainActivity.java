8ac62964fe042e878883d235ac9d36f957b95179
haha <1904708437@qq.com>
2016-06-24 16:46:33 +0800

    Initial commit

---
+++ app/src/main/java/com/oasis/customseekbar/MainActivity.java
@@ -0,0 +1,19 @@
+package com.oasis.customseekbar;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+
+    }
+
+
+}
